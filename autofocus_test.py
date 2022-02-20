b92f0486167b32f4a262478b8a30357f5cd1987c
Tomas 'Jethro' Pokorny <xtompok@gmail.com>
2022-02-20 21:29:36 +0100

    Better autofocus, fixed ramdisk script.

--- autofocus_test.py
+++ autofocus_test.py
@@ -6,6 +6,9 @@ from capture import gstreamer_pipeline
 import time
 import cv2
 
+SUFFICIENT_FOCUS = 80
+CROASE_STEPS = 10
+
 
 def find_peak(vals,nsteps):
     bestup = None
@@ -26,7 +29,9 @@ def find_peak(vals,nsteps):
                     bestdown = ratio
                     bestidx = idx+1
         memratio = ratio
-    return (bestidx)/nsteps
+    if bestidx is None:
+        return None,None
+    return (bestidx)/nsteps,vals[bestidx]
 
 def fine_focus_recursive(ctl,cam,minfocus,maxfocus,mindiff=0.01):
     if (maxfocus-minfocus < mindiff):
@@ -37,9 +42,9 @@ def fine_focus_recursive(ctl,cam,minfocus,maxfocus,mindiff=0.01):
     focus1 = max((minfocus+maxfocus)/2-step/2,0)
     focus2 = min((minfocus+maxfocus)/2+step/2,1)
     
-    ret,frame = get_frame_focus(ctl,cam,focus1)
+    ret,frame = get_frame_at_focus(ctl,cam,focus1)
     laplac1 = laplacian2(frame)
-    ret,frame = get_frame_focus(ctl,cam,focus2)
+    ret,frame = get_frame_at_focus(ctl,cam,focus2)
     laplac2 = laplacian2(frame)
 
     print(f"Min: {minfocus:.3f}, max: {maxfocus:.3f}, F1: {focus1:.3f}, F2: {focus2:.3f}, lap1: {laplac1}, lap2: {laplac2}")
@@ -62,10 +67,13 @@ def fine_focus(ctl, cam, bestcroase,width,steps=10):
         focus = minfocus + width*i/steps
         focus = min(focus,1)
         
-        ret,frame = get_frame_focus(ctl,cam,focus)
+        ret,frame = get_frame_at_focus(ctl,cam,focus)
         laplac = laplacian2(frame)
         laplacs.append(laplac)
-        print(f"Focus: {focus}, laplac2: {laplac}")
+#        print(f"Focus: {focus}, laplac2: {laplac}")
+        
+        if laplac > SUFFICIENT_FOCUS:
+            return focus,laplac
 
         if laplac > bestlaplac:
             bestlaplac = laplac
@@ -77,34 +85,39 @@ def fine_focus(ctl, cam, bestcroase,width,steps=10):
     laplacs.sort()
     threshold = laplacs[-2]*1.5
 
-    focus = max(bestfocus-0.05,0)
-    ret,frame = get_frame_focus(ctl,cam,focus)
+    focus = max(bestfocus-0.02,0)
+    ret,frame = get_frame_at_focus(ctl,cam,focus)
     memlaplac = laplacian2(frame)
-    while focus < bestfocus+0.05:
+#    print(f"Focus: {focus}, Memlaplac: {memlaplac}")
+    while focus < bestfocus+0.02:
         
-        ret,frame = get_frame_focus(ctl,cam,focus)
+        ret,frame = get_frame_at_focus(ctl,cam,focus)
         laplac = laplacian2(frame)
 
-        print(f"Focus: {focus}, laplac2: {laplac}")
-        print(f"memlaplac: {memlaplac}, threshold: {threshold}")
+#        print(f"Focus: {focus}, laplac2: {laplac}")
 
-        if memlaplac > laplac and memlaplac > threshold:
-            ret,frame = get_frame_focus(ctl,cam,focus-0.025)
+        if laplac > SUFFICIENT_FOCUS:
+            return focus,laplac
+
+        if memlaplac-5 > laplac and memlaplac > threshold:
+            ctl.set_focus(focus-0.05)
+            final = focus-0.005
+            ret,frame = get_frame_at_focus(ctl,cam,final)
             laplac = laplacian2(frame)
 
-            print(f"Final Focus: {focus-0.025}, laplac2: {laplac}")
-            return
+            print(f"Final Focus: {final}, laplac2: {laplac}")
+            return final,laplac
 
         focus +=0.005
-        print(memlaplac,laplac)
         memlaplac = laplac
         
         if focus > 1:
-            return
+            return focus,laplac
+    return None, None
 
 
 
-def get_frame_focus(ctl,cam,focus):
+def get_frame_at_focus(ctl,cam,focus):
         ctl.set_focus(focus)
         ctl.waiting_for_free()
         for _ in range(12):
@@ -112,7 +125,22 @@ def get_frame_focus(ctl,cam,focus):
         
         return cam.read()
 
+def croase_focus(ctl,cam):
+        croase = []
+        for f in range(CROASE_STEPS):
+            ret,frame = get_frame_at_focus(ctl,cam,f/10) 
+            croase.append(laplacian2(frame))
+        
+        print(croase)
+        return find_peak(croase,CROASE_STEPS)
 
+
+def autofocus(ctl,cam):
+    croase,val = croase_focus(ctl,cam)
+    if croase is None:
+        return None,None
+    fine,val = fine_focus(ctl,cam,croase,2/CROASE_STEPS)
+    return fine,val
         
 
 def main():
@@ -122,23 +150,19 @@ def main():
     cam = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
 
 
-    for z in range(30,100):
+    for z in range(0,100):
         ctl.set_zoom(z/100)
         print(f"Zoom: {z/100}")
         
-        croase = []
-        for f in range(10):
-            ret,frame = get_frame_focus(ctl,cam,f/10) 
-            croase.append(laplacian2(frame))
 
             #print(f"Z: {z}, F: {f}, sobel: {sobel(frame)}, laplacian: {laplacian(frame)}, laplacian2: {laplacian2(frame)}")
 
-        print(croase)
-        bestfocus = find_peak(croase,10)
-        print(f"Best croase: {bestfocus}")
-        fine_focus(ctl,cam,bestfocus,0.2)
-
-        fine_focus_recursive(ctl,cam,bestfocus-0.1, bestfocus+0.1)
+        focus,val = autofocus(ctl,cam)
+        print(f"Focus: {focus}, laplac: {val}")
+        ret,frame = cam.read()
+        print(ret,int(time.time()))
+        cv2.imwrite(f'/tmp/ram/{int(time.time())}.jpg',frame)
+        #fine_focus_recursive(ctl,cam,bestfocus-0.1, bestfocus+0.1)
 
 
 
--- temp.sh
+++ temp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+mkdir /tmp/ram
 sudo mount -t ramfs -o size=2g ramfs /tmp/ram
 sudo chown jethro:jethro /tmp/ram
