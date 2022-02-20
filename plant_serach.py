acce5e0ccc69834e66bc2bda5138423a8aa61314
Tomas 'Jethro' Pokorny <xtompok@gmail.com>
2022-02-21 00:21:11 +0100

    Added S3 upload, CSV logging and much more.

--- plant_serach.py
+++ plant_serach.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 import cv2
-from capture import gstreamer_pipeline
+from capture import open_camera
 from control import Controller
+from autofocus import autofocus 
+from s3 import load_key 
+import boto3
 import time
 from pathlib import Path
 import click
 import requests
 import base64
+import csv
+import pprint
 
 STEP = 5
 DELAY = 0.5
@@ -15,13 +20,61 @@ MIN_TILT = 15
 MAX_TILT = 165
 MIN_PAN = 0
 MAX_PAN = 180
+ZOOM = 0.7
 
 IMG_MAX_RETRY = 5
 
-STORAGE_DIR = Path('/tmp/ram')
+PREFIX = 'test'
+BUCKET = 'plantid-origami'
 
-def generate_filename(pan, tilt, ir=False):
-    return Path(f"{pan}_{tilt}_ir{int(ir)}_{int(time.time()*10)}.jpg")
+STORAGE_DIR = Path('/tmp/ram')
+LOG_CSV_FILENAME = STORAGE_DIR / Path('results.csv')
+LOG_FIELDS = ('timestamp','prefix','pan','tilt','zoom','focus','laplacian',
+'pid_id',
+'pid_plant_name',
+'pid_probability',
+'pid_is_plant_probability',
+'pid_is_healthy_probability',
+'pid_diseases_0_entity_id',
+'pid_diseases_0_probability',
+'pid_diseases_0_name',
+'pid_diseases_1_entity_id',
+'pid_diseases_1_probability',
+'pid_diseases_1_name',)
+
+pp = pprint.PrettyPrinter(width=200)
+
+def generate_filename(ctl,timestamp,prefix):
+    name = f'p{prefix}_t{timestamp}_P{ctl.get_pan()}_T{ctl.get_tilt()}_Z{int(ctl.get_zoom()*100)}_F{int(ctl.get_focus()*10000)}.jpg'
+    return Path(name)
+
+def append_to_log(ctl,timestamp,prefix,laplacian,plant_res):
+    data = {
+            'timestamp': timestamp,
+            'prefix': prefix,
+            'pan': ctl.get_pan(),
+            'tilt': ctl.get_tilt(),
+            'zoom': ctl.get_zoom(),
+            'focus': ctl.get_focus(),
+            'laplacian': laplacian,
+            'pid_id': plant_res['id'],
+            'pid_plant_name': plant_res['suggestions'][0]['plant_name'],
+            'pid_probability': plant_res['suggestions'][0]['probability'],
+            'pid_is_plant_probability': plant_res['is_plant_probability'],
+            'pid_is_healthy_probability': plant_res['health_assessment']['is_healthy_probability'],
+            'pid_diseases_0_entity_id': plant_res['health_assessment']['diseases'][0]['entity_id'],
+            'pid_diseases_0_probability': plant_res['health_assessment']['diseases'][0]['probability'],
+            'pid_diseases_0_name': plant_res['health_assessment']['diseases'][0]['name'],
+            'pid_diseases_1_entity_id': plant_res['health_assessment']['diseases'][1]['entity_id'],
+            'pid_diseases_1_probability': plant_res['health_assessment']['diseases'][1]['probability'],
+            'pid_diseases_1_name': plant_res['health_assessment']['diseases'][1]['name'],
+
+            }
+    with open(LOG_CSV_FILENAME,'a') as f:
+        writer = csv.DictWriter(f,fieldnames = LOG_FIELDS)
+        writer.writerow(data)
+
+
 
 def encode_file(file_name):
     with open(file_name, "rb") as file:
@@ -66,48 +119,73 @@ def identify_plant(filename,api_key):
 
 
 
-def capture(ctl, cam, api_key, pan,tilt,ir,delay):
+def capture(ctl, cam, pan, tilt, zoom, api_key, s3cli):
     ctl.set_tilt(tilt)
     time.sleep(0.01)
     ctl.set_pan(pan)
-    time.sleep(delay)
+    time.sleep(0.01)
+    ctl.set_zoom(zoom)
+    time.sleep(0.01)
+
+    focus, laplacian = autofocus(ctl,cam)
     
     for _ in range(IMG_MAX_RETRY):
         ret,frame = cam.read()
+        timestamp = int(time.time())
         if ret:
             break
     else:
         print("Failed to capture image")
 
-    filepath = STORAGE_DIR / generate_filename(pan,tilt,ir)
+    filepath = STORAGE_DIR / generate_filename(ctl, timestamp, PREFIX)
 
     cv2.imwrite(filepath.as_posix(),frame)
+    
+    s3cli.upload_file(filepath.as_posix(),BUCKET,f'{PREFIX}/{filepath.name}')
+    
     result = identify_plant(filepath,api_key)
+    pp.pprint(result)
     print(f"Is plant: {result['is_plant']} with probability {result['is_plant_probability']}")
+    
+    append_to_log(ctl,timestamp,PREFIX,laplacian,result)
+    s3cli.upload_file(LOG_CSV_FILENAME.as_posix(),BUCKET,f'{PREFIX}/{LOG_CSV_FILENAME.name}')
+
 
 
 
 @click.command()
 @click.option('--apikey', help='Path to the file with API key', required=True, type=click.File('r'))
-def main(apikey):
+@click.option('--s3key', help='Path to the file with S3 key', required=True, type=click.File('r'))
+@click.option('--prefix', help='Prefix for captured data')
+def main(apikey,s3key,prefix):
+    # Process arguments
+    if prefix is not None:
+        PREFIX = prefix
 
     apikey = apikey.read().strip()
 
+    access,secret = load_key(s3key)
+    s3cli = boto3.client('s3',aws_access_key_id = access, aws_secret_access_key = secret)
+
+    # Prepare camera and controller
     ctl = Controller(1)
     ctl.print_status()
 
-    cam = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+    cam = open_camera() 
 
+    ctl.set_zoom(ZOOM)
+
+    # Scan
     for tilt in range(MIN_TILT,MAX_TILT,2*STEP):
         for pan in range(MIN_PAN,MAX_PAN,STEP):
             print(f"Pan: {pan}, tilt: {tilt}")
-            capture(ctl,cam,apikey,pan,tilt,False,DELAY)
+            capture(ctl,cam,pan,tilt,ZOOM,apikey,s3cli)
 
         tilt += STEP
         time.sleep(0.01)
         for pan in range(MAX_PAN,MIN_PAN,-STEP):
             print(f"Pan: {pan}, tilt: {tilt}")
-            capture(ctl,cam,apikey,pan,tilt,False,DELAY)
+            capture(ctl,cam,pan,tilt,ZOOM,apikey,s3cli)
 
 
 if __name__ == '__main__':
